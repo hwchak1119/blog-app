944409859cd20601fef0b6874a0f3753e732bdfc
hwchak1119 <hwchak1119@gmail.com>
2022-08-03 21:41:09 +0800

    Setup

---
+++ pages/index.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const posts = [
+  { title: "React Testing", excerpt: "Learn React Testing" },
+  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
+];
+
+const Home: NextPage = () => {
+  return (
+    <div className='container mx-auto px-10 mb-8 bg-gray-300'>
+      <Head>
+        <title>Blog App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='col-span-1 lg:col-span-8'>
+          {posts.map((post, index) => (
+            <>
+              {post.title}
+              {post.excerpt}
+            </>
+          ))}
+        </div>
+        <div className='col-span-1 lg:col-span-4'>
+          <div className='relative top-8 lg:sticky'>hi</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
