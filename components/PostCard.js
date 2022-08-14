69b10d3c89d6dfb4acc2062d59fbefe962108366
hwchak1119 <hwchak1119@gmail.com>
2022-08-14 19:50:00 +0800

    Image cover

--- components/PostCard.js
+++ components/PostCard.js
@@ -5,13 +5,12 @@ import "material-symbols/rounded.css";
 
 function PostCard({ post }) {
   return (
-    <div className="group cursor-pointer bg-white shadow-lg hover:shadow-2xl shadow-blue-900/50 transition duration-200 rounded-md p-0 lg:p-8 pb-12 mb-8">
-      <div className="relative overflow-hidden shadow-md pb-80 mb-6">
-        {post?.auther}
+    <div className="group cursor-pointer bg-white shadow-lg hover:shadow-2xl shadow-blue-900/50 transition duration-200 rounded-md overflow-hidden p-0 lg:p-8 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md mb-6">
         <img
           src={post?.featuredImage.url}
           alt="post.title"
-          className="object-top absolute h-80 w-full object-cover shadow-sm rounded-t-md lg:rounded-md"
+          className="h-80 w-full object-cover shadow-sm lg:rounded-md"
         />
       </div>
       <h1 className="transition duration-200 text-center mb-8 cursor-pointer group-hover:text-pink-600 text-3xl font-semibold">
@@ -19,19 +18,17 @@ function PostCard({ post }) {
       </h1>
       <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
         <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-4">
-          <img
-            src={post?.author?.photo.url}
-            alt={post?.author?.name}
-            width="30px"
-            height="30px"
-            className="align-middle rounded-full"
-          />
-          <p className="inline align-middle text-gray-700 ml-2 text-lg">
-            {post.author.name}
-          </p>
+          <div className="rounded-full overflow-hidden w-8 h-8">
+            <img
+              src={post?.author?.photo.url}
+              alt={post?.author?.name}
+              className="object-cover h-8"
+            />
+          </div>
+          <p className="text-gray-700 ml-2 text-lg">{post.author.name}</p>
         </div>
         <div className="inline-flex items-center gap-1 font-medium text-gray-700">
-          <span class="material-symbols-rounded">schedule</span>{" "}
+          <span class="material-symbols-rounded">schedule</span>
           <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
         </div>
       </div>
@@ -40,7 +37,7 @@ function PostCard({ post }) {
       </p>
       <div className="text-center">
         <Link href={`/post/${post.slug}`}>
-          <span className="transition duration-200 transform hover:translate-y-1 hover:shadow-md hover:shadow-pink-500/50 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3">
+          <span className="transition duration-200 transform hover:-translate-y-1 hover:shadow-md hover:shadow-pink-500/50 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3">
             View more
           </span>
         </Link>
