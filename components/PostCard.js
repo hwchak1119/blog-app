1cd77afb182902316aabc9e5040017062d71f596
hwchak1119 <hwchak1119@gmail.com>
2022-08-14 18:42:58 +0800

    Post Card Layout

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,19 +1,49 @@
 import React from "react";
 import moment from "moment";
 import Link from "next/link";
+import "material-symbols/rounded.css";
 
 function PostCard({ post }) {
   return (
-    <div>
-      <div className='bg-white shadow-lg rounded-md p-0 lg:p-8 pb-12 mb-8'>
-        <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
-          {post.auther}
+    <div className="group cursor-pointer bg-white shadow-lg hover:shadow-2xl shadow-blue-900/50 transition duration-200 rounded-md p-0 lg:p-8 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+        {post?.auther}
+        <img
+          src={post?.featuredImage.url}
+          alt="post.title"
+          className="object-top absolute h-80 w-full object-cover shadow-sm rounded-t-md lg:rounded-md"
+        />
+      </div>
+      <h1 className="transition duration-200 text-center mb-8 cursor-pointer group-hover:text-pink-600 text-3xl font-semibold">
+        <Link href={`/post/${post?.slug}`}>{post?.title}</Link>
+      </h1>
+      <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
+        <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-4">
           <img
-            src={post.featuredImage.url}
-            alt='post.title'
-            className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-md lg:rounded-md'
+            src={post?.author?.photo.url}
+            alt={post?.author?.name}
+            width="30px"
+            height="30px"
+            className="align-middle rounded-full"
           />
+          <p className="inline align-middle text-gray-700 ml-2 text-lg">
+            {post.author.name}
+          </p>
         </div>
+        <div className="inline-flex items-center gap-1 font-medium text-gray-700">
+          <span class="material-symbols-rounded">schedule</span>{" "}
+          <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
+        </div>
+      </div>
+      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
+        {post.excerpt}
+      </p>
+      <div className="text-center">
+        <Link href={`/post/${post.slug}`}>
+          <span className="transition duration-200 transform hover:translate-y-1 hover:shadow-md hover:shadow-pink-500/50 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3">
+            View more
+          </span>
+        </Link>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,19 +5,19 @@ import { getPosts } from "../services";
 
 const Home: NextPage = ({ posts }: any) => {
   return (
-    <div className='container mx-auto px-10 mb-8'>
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>Blog App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='col-span-1 lg:col-span-8'>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
           {posts.map((post: any) => {
             return <PostCard post={post.node} key={post.node.title} />;
           })}
         </div>
-        <div className='col-span-1 lg:col-span-4'>
-          <div className='relative top-8 lg:sticky'>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
             <PostWidget />
             <Categories />
           </div>
