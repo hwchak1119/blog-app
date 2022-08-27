cc88226d831dccd3602fed7da4d1acd89aa6cb13
hwchak1119 <hwchak1119@gmail.com>
2022-08-27 13:07:03 +0800

    Create post detail page

---
+++ pages/post/[slug].js
@@ -0,0 +1,16 @@
+import React from "react";
+import { getPosts, getPostDetails } from "../../services";
+import {
+  PostDetail,
+  Categories,
+  PostWidget,
+  Author,
+  Comments,
+  CommentForm,
+} from "../../components";
+
+const PostDetails = () => {
+  return <div>PostDetails</div>;
+};
+
+export default PostDetails;
