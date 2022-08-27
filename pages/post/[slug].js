bdd66872f461a3d2f64db55feb824ee8e4ccfbb8
hwchak1119 <hwchak1119@gmail.com>
2022-08-27 13:20:32 +0800

    Post detail layout

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -10,7 +10,22 @@ import {
 } from "../../components";
 
 const PostDetails = () => {
-  return <div>PostDetails</div>;
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-col-1 lg:grid-col-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
+          {/* <PostDetail />
+          <Author />
+          <CommentForm />
+          <Comments /> */}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <PostWidget />
+          <Categories />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostDetails;
