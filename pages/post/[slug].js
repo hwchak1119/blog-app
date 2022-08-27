d877e8a8769a8ac1771e0309d8f463a1bebf39b9
hwchak1119 <hwchak1119@gmail.com>
2022-08-27 13:31:47 +0800

    Create all components

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -12,12 +12,12 @@ import {
 const PostDetails = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="grid grid-col-1 lg:grid-col-12 gap-12">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
-          {/* <PostDetail />
+          <PostDetail />
           <Author />
           <CommentForm />
-          <Comments /> */}
+          <Comments />
         </div>
         <div className="col-span-1 lg:col-span-4">
           <PostWidget />
