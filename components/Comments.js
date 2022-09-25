60cd414a9d5932c44bb4b475ede0f5be00b4efa3
hwchak1119 <hwchak1119@gmail.com>
2022-09-25 20:34:22 +0800

    Comment entry box

--- components/Comments.js
+++ components/Comments.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-const Comments = () => {
+const Comments = ({ slug }) => {
+  const [error, seterror] = useState(false);
+  const [localStorage, setlocalStorage] = useState(null);
+  const [showSuccessMessage, setshowSuccessMessage] = useState(false);
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
   return (
-    <div>
-      <h1>Comments</h1>
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Comments</h3>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <textarea
+          ref={commentEl}
+          className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+        />
+      </div>
     </div>
   );
 };
